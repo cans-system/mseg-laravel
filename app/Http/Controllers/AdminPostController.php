16f99c95d227b6e508311fe964e322c3e5e2939a
yamaneko05 <o.yamaneko0331@gmail.com>
2024-07-01 15:57:09 +0900

    adminPostController 並び順 修正

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -11,7 +11,7 @@ class AdminPostController extends Controller
 {
     public function index() {
         return view('pages.admin.posts', [
-            'posts' => Post::latest()->get()
+            'posts' => Post::orderBy('published_at', 'desc')->get()
         ]);
     }
     
